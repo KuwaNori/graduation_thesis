9d979af9d29b72b7cca4b708c07bf3193a9c5bbf
KuwaNori <kuwanori4778@gmail.com>
2021-06-08 15:39:36 +0900

    dd bootstrap links

--- view/news.php
+++ view/news.php
@@ -22,13 +22,13 @@
     <meta charset="utf-8">
     <link rel="stylesheet" href="./css/news.css">
     <link rel="stylesheet" href="./css/header.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <title></title>
   </head>
   <body>
     <header>
         <form class="header-top" method="post" action="#" name="word">
           <input type="text" class="text-input" name="input_company_name">
-          <input type="hidden" name="token" value="<?php $token; ?>" >
           <label for="search">
             <div class="search">
               <img class="search-img" src="../img/search.svg" alt="">
@@ -57,4 +57,7 @@
       </div>
     </main>
   </body>
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 </html>
