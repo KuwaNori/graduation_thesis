01a4c3855b318b3566ec6ea57471e23a6c79a16b
KuwaNori <kuwanori4778@gmail.com>
2021-06-14 11:43:50 +0900

    let getlist.php compare the result

--- control/getlist.php
+++ control/getlist.php
@@ -10,7 +10,7 @@ $dbconn = pg_connect("host=$personal[0] dbname=$personal[1] user=$personal[2] pa
 
 function getNewsList($company_name){
   $rel = getComapnyId($company_name);
-  $company_id = $rel[0];
+  $company_id = $rel["company_id"];
   if($company_id != ""){
     $sql_getnews = "select * from news where company_id = {$company_id};";
     $result = pg_query($sql_getnews) or die('Query failed: ' . pg_last_error());
@@ -24,8 +24,12 @@ function getNewsList($company_name){
 function getComapnyId($company_name){
   $sql_getcompanyid = "select * from companys where company_name like '%{$company_name}%'";
   $result = pg_query($sql_getcompanyid) or die('Query failed: ' . pg_last_error());
-  $result = pg_fetch_row($result);
-  return $result;
+  $case = pg_fetch_all($result);
+  var_dump(count($case));
+  if (count($case) == 1){
+    return $case[0];
+  }
+  return $case;
 }
 
  ?>
